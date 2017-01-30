90d92f408ee27286d97be3fe14fed2c064c7154f
junifar <junifar@gmail.com>
2017-01-30 12:41:57 +0700

    rukun warga

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::post('/kelurahan/save', 'KelurahanController@save');
 Route::post('/kelurahan/update/{id}', 'KelurahanController@update');
 
 Route::get('/rukunwarga','RukunWargaController@index');
+Route::get('/rukunwarga/add','RukunWargaController@insert');
 
 Auth::routes();
 
