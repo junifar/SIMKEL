2f661feacd1ab772d7cc3f00a0eed5b4fa3a57de
junifar <junifar@gmail.com>
2017-01-31 10:30:18 +0700

    adding state save adding loading image button add

--- resources/views/kelurahan/view.blade.php
+++ resources/views/kelurahan/view.blade.php
@@ -11,6 +11,13 @@
 @endsection
 
 @section('content')
+    <section class="content-header">
+        <h1>
+            Kelurahan
+            <small>List data</small>
+            <a href="/kelurahan/add" class="btn btn-primary pull-right">New Data</a>
+        </h1>
+    </section>
     <section class="content">
         <div class="row">
             <div class="col-md-12">
@@ -35,21 +42,21 @@
 <script>
     $(document).ready(function(){
        $('#kelurahan-table').DataTable({
-                processing: true,
-               "language":{
-                   "processing": '<img src="{{ url('/images/ajax-loader.gif') }}"/><br/>Load Data...',
-               },
-                serverSide: true,
-                ajax: '{{ url('/kelurahan/view-data') }}',
-                stateSave: true,
-                columns: [
-                    { data: 'name', name: 'name' },
-                    { data: 'lurah', name: 'lurah' },
-                    { data: 'phone', name: 'phone' },
-                    { data: 'address', name: 'address' },
-                    { data: 'action', name: 'action', orderable: false, searchable: false}
-                ]
-            });
+            processing: true,
+            "language":{
+               "processing": '<img src="{{ url('/images/ajax-loader.gif') }}"/><br/>Load Data...',
+            },
+            serverSide: true,
+            ajax: '{{ url('/kelurahan/view-data') }}',
+            stateSave: true,
+            columns: [
+                { data: 'name', name: 'name' },
+                { data: 'lurah', name: 'lurah' },
+                { data: 'phone', name: 'phone' },
+                { data: 'address', name: 'address' },
+                { data: 'action', name: 'action', orderable: false, searchable: false}
+            ]
+       });
     });
 </script>
 @endpush
